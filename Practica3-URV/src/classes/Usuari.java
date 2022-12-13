4a425124ec0f04d9049eeeb7e81e5280b7c090be
Germán Puerto <germanangel.puerto@estudiants.urv.cat>
2022-12-13 17:28:47 +0100

    Manejo de usuarios

--- Practica3-URV/src/classes/Usuari.java
+++ Practica3-URV/src/classes/Usuari.java
@@ -1,10 +1,14 @@
 package classes;
 
+import lists.LlistaPeticionsIntercanvi;
+import lists.LlistaProductes;
+
 public class Usuari{
 
     private String alies, email;
     private int codiPostal;
-
+    //private LlistaPeticionsIntercanvi intercanvisUsuari;        // LLista per guardar intercanvis fets
+    //private LlistaProductes productesUsuari;                    // Productes que ofereix l'usuari
     //TODO Guardar els productes que ofereix i els intercanvis 
 
     public Usuari(String alies, String email, int codiPostal) {
@@ -12,4 +16,9 @@ public class Usuari{
         this.email = email;
         this.codiPostal = codiPostal;
     }
+
+    public Usuari(Usuari usuari) {
+        this(usuari.alies, usuari.email, usuari.codiPostal);
+    }
+   
 }
--- Practica3-URV/src/lists/LlistaUsuaris.java
+++ Practica3-URV/src/lists/LlistaUsuaris.java
@@ -3,5 +3,17 @@ package lists;
 import classes.Usuari;
 
 public class LlistaUsuaris {
-    
+    private Usuari[] llista;
+    private int numUsuaris;
+
+
+    public LlistaUsuaris(int mida) {
+        llista = new Usuari[mida];
+        this.numUsuaris = 0;
+    }
+
+    public void AfegirUsuari(Usuari currUsuari) {
+        llista[numUsuaris] = new Usuari(currUsuari);
+        numUsuaris++;
+    }
 }
