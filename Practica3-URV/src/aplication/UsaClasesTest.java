ea08cbc0a5672975ad827062b40aa78de8cd83f9
Germán Puerto <germanangel.puerto@estudiants.urv.cat>
2022-12-13 17:27:56 +0100

    Manejo de los usuarios

--- Practica3-URV/src/aplication/UsaClasesTest.java
+++ Practica3-URV/src/aplication/UsaClasesTest.java
@@ -1,11 +1,50 @@
 package aplication;
 
+import java.io.*;
+import java.util.Scanner;
+
 import classes.*;
+import lists.LlistaUsuaris;
 
 public class UsaClasesTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException{
+			Scanner fitxerUsuaris = new Scanner(new File("elcolega.txt"));
+			//ObjectOutputStream fitxerSortida = new ObjectOutputStream(new FileOutputStream("Densitat.bin"));
 
-		System.out.println("Hello, World!");
+
+			LlistaUsuaris llistaUsers = new LlistaUsuaris(2);
+			String usuari;
+
+			while(fitxerUsuaris.hasNextLine()){
+				usuari = fitxerUsuaris.nextLine();
+				readUsuaris(usuari, llistaUsers);
+				System.out.println(usuari);
+			}
+
+
+			fitxerUsuaris.close();
 	
 	}
+	public static void readUsuaris(String usuari, LlistaUsuaris llistaUsers) {
+		Scanner atributtes = new Scanner(usuari);
+		atributtes.useDelimiter(";");
+
+		String alies, email;
+		int codiPostal;
+
+		alies = atributtes.next();
+		email = atributtes.next();
+		codiPostal = Integer.parseInt(atributtes.next());
+
+		Usuari currUser = new Usuari(alies, email, codiPostal);
+		llistaUsers.AfegirUsuari(currUser);
+
+		
+		atributtes.close();
+	}
+	public static void readProductes() {
+
+	}
 }
+
+
